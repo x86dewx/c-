5e030dd347aac6d4a00c1327430c18a7b579fbe1
x86dewx <3436057160@qq.com>
2024-09-10 20:11:23 +0800

    Add files

--- WANGXIN/20240910/framebuffer.h
+++ WANGXIN/20240910/framebuffer.h
@@ -9,6 +9,9 @@ extern void uninitfd(int fd);
 void drow_point(int x,int y,int col);
 void drow_horizontal_line(int x,int y,int len,unsigned int col);
 void drow_vertical_line(int x,int y,int len,unsigned int col);
-
+void drow_squre(int x,int y,int a,int b,unsigned int col);
+void drow_oblique(int x,int y,int len,float oblique,unsigned col);
+void clear(unsigned int col);
+void drow_circle(int x,int y,int r,unsigned int col);
 
 #endif
--- WANGXIN/20240910/main.c
+++ WANGXIN/20240910/main.c
@@ -1,6 +1,6 @@
 #include"framebuffer.h"
 #define red 0x00ff0000
-
+#define black 0x00000000
 
 
 
@@ -11,9 +11,18 @@ int main(int argc, char *argv[])
     {
         return -1;
     }
+    clear(black);
     drow_horizontal_line(200,300,400,red);
-    drow_horizontal_line(400,150,300,red);
+    drow_vertical_line(400,150,300,red);
+    drow_squre(250,150,300,300,red);
+    drow_oblique(400,300,300,1,red);
+    drow_oblique(400,300,300,-1,red);
+    drow_oblique(100,600,300,-1,red);
+    drow_oblique(100,0,300,1,red);
+
+    drow_circle(400,300,300,red);
     uninitfd(fd);
+    while(1);
     return 0;
 }
 
